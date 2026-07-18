cd7ec79b3029f9549ae2d9590476a534a2a94ec6
Ivan_laptop <sementsov0904@gmail.com>
2026-07-18 17:39:10 +0900

    Фикс путей

--- frontend/src/components/PriceList/PriceListModal.jsx
+++ frontend/src/components/PriceList/PriceListModal.jsx
@@ -1,6 +1,6 @@
 // src/components/PriceListModal.jsx
 import React, { useState, useEffect } from "react";
-import { api } from "../api/apiService";
+import { api } from "../api/apiService.js";
 import "./PriceListModal.css";
 
 // Карточки классов с народными примерами и путями к созданным иконкам
